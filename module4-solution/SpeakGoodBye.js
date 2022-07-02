b1ba8fcb6a98655cdfbaeb37aa53958de2646c80
wmoschell <williammoschell@iMac.local>
2022-07-01 14:54:02 -1000

    module 4 update

--- module4-solution/SpeakGoodBye.js
+++ module4-solution/SpeakGoodBye.js
@@ -13,7 +13,7 @@
 var byeSpeaker = {};
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye ";
+var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -10,7 +10,7 @@ var helloSpeaker = {};
 
 
 // DO NOT attach the speakWord variable to the 'helloSpeaker' object.
-var speakWord = "Hello ";
+var speakWord = "Hello";
 
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
